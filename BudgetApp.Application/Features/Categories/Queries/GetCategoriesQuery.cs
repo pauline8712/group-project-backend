4058cc9fcec4c8762386bc620dda29659797d4f3
Pauline <plinekaeorg@gmail.com>
2026-05-09 17:30:19 +0200

    Add GetCategoriesQuery for retrieving categories by BudgetId

---
+++ BudgetApp.Application/Features/Categories/Queries/GetCategoriesQuery.cs
@@ -0,0 +1,10 @@
+﻿using BudgetApp.Application.Features.Categories.DTOs;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Categories.Queries
+{
+   public class GetCategoriesQuery : IRequest<List<CategoryDto>>
+    {
+        public Guid BudgetId { get; set; }
+    }
+}
