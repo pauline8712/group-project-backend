137acf65a2e8ac8347bac6a11162f5116e67aeb7
Pauline <plinekaeorg@gmail.com>
2026-05-09 18:05:04 +0200

    Add CreateCategoryCommandHandler for creating a new category with initial balance

---
+++ BudgetApp.Application/Features/Categories/Commands/CreateCategoryCommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using BudgetApp.Application.Features.Categories.DTOs;
+using BudgetApp.Application.Interfaces;
+using BudgetApp.Domain.Entities;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Categories.Commands;
+
+public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, CategoryDto>
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public CreateCategoryCommandHandler(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<CategoryDto> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = new Category
+        {
+            Id = Guid.NewGuid(),
+            BudgetId = request.BudgetId,
+            Name = request.Name,
+            AllocatedAmount = request.AllocatedAmount,
+            CurrentBalance = request.AllocatedAmount,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        var created = await _categoryRepository.AddAsync(category);
+
+        return new CategoryDto
+        {
+            Id = created.Id,
+            BudgetId = created.BudgetId,
+            Name = created.Name,
+            AllocatedAmount = created.AllocatedAmount,
+            CurrentBalance = created.CurrentBalance,
+            CreatedAt = created.CreatedAt
+        };
+    }
+}
