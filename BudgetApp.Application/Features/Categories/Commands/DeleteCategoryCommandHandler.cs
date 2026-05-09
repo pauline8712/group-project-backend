cba1f309fe633210398770e842a70799c7a58e48
Pauline <plinekaeorg@gmail.com>
2026-05-09 18:19:24 +0200

    Add DeleteCategoryCommandHandler for deleting a category by Id

---
+++ BudgetApp.Application/Features/Categories/Commands/DeleteCategoryCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using BudgetApp.Application.Interfaces;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Categories.Commands;
+
+public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand, bool>
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public DeleteCategoryCommandHandler(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<bool> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
+    {
+        var category = await _categoryRepository.GetByIdAsync(request.Id);
+
+        if (category == null)
+            return false;
+
+        await _categoryRepository.DeleteAsync(request.Id);
+        return true;
+    }
+}
