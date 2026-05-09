cdac25da608bc93d5d1d49d6a5f78240ecb86124
Pauline <plinekaeorg@gmail.com>
2026-05-09 18:00:29 +0200

    Add GetCategoriesQueryHandler and GetCategoryByIdQuery for Category read operations

--- BudgetApp.Application/Features/Categories/Queries/GetCategoriesQueryHandler.cs
+++ BudgetApp.Application/Features/Categories/Queries/GetCategoriesQueryHandler.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BudgetApp.Application.Features.Categories.DTOs;
+using BudgetApp.Application.Interfaces;
+using MediatR;
 
-namespace BudgetApp.Application.Features.Categories.Queries
+namespace BudgetApp.Application.Features.Categories.Queries;
+
+public class GetCategoriesQueryHandler : IRequestHandler<GetCategoriesQuery, List<CategoryDto>>
 {
-    internal class GetCategoriesQueryHandler
+    private readonly ICategoryRepository _categoryRepository;
+
+    public GetCategoriesQueryHandler(ICategoryRepository categoryRepository)
     {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<List<CategoryDto>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        var categories = await _categoryRepository.GetAllAsync(request.BudgetId);
+
+        return categories.Select(c => new CategoryDto
+        {
+            Id = c.Id,
+            BudgetId = c.BudgetId,
+            Name = c.Name,
+            AllocatedAmount = c.AllocatedAmount,
+            CurrentBalance = c.CurrentBalance,
+            CreatedAt = c.CreatedAt
+        }).ToList();
     }
 }
---
+++ BudgetApp.Application/Features/Categories/Queries/GetCategoryByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using BudgetApp.Application.Features.Categories.DTOs;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Categories.Queries;
+
+public class GetCategoryByIdQuery : IRequest<CategoryDto?>
+{
+    public Guid Id { get; set; }
+}
