51be32f3e02424f8163cd103f2a2016c9ebd84d7
Pauline <plinekaeorg@gmail.com>
2026-05-09 15:24:09 +0200

    I added OnModelCreating in the AppDbContext to be more specific for the database. To configurate relationships and foreign keys for user, budget, category and transaction. Theres already Navigation properties in the models that are telling what is connected to what that is used in the code when they are getting data with Include(). It complement each other

--- BudgetApp.API/Program.cs
+++ BudgetApp.API/Program.cs
@@ -1,3 +1,4 @@
+using BudgetApp.Infrastructure;
 
 namespace BudgetApp.API
 {
@@ -13,6 +14,7 @@ namespace BudgetApp.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddInfrastructure(builder.Configuration);
 
             var app = builder.Build();
 
---
+++ BudgetApp.Infrastructure/Database/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using BudgetApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetApp.Infrastructure.Database;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Budget> Budgets { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Transaction> Transactions { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // User → Budget (1 till 1)
+        modelBuilder.Entity<Budget>()
+            .HasOne(b => b.User)
+            .WithOne(u => u.Budget)
+            .HasForeignKey<Budget>(b => b.UserId);
+
+        // Budget → Categories (1 till N)
+        modelBuilder.Entity<Category>()
+            .HasOne(c => c.Budget)
+            .WithMany(b => b.Categories)
+            .HasForeignKey(c => c.BudgetId);
+
+        // Category → Transactions (1 till N)
+        modelBuilder.Entity<Transaction>()
+            .HasOne(t => t.Category)
+            .WithMany(c => c.Transactions)
+            .HasForeignKey(t => t.CategoryId);
+
+       
+    }
+}
