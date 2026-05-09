33113b76ad76a3567ee86ac4a89cb590f43122c8
Pauline <plinekaeorg@gmail.com>
2026-05-09 18:03:43 +0200

    Add CreateCategoryCommand for creating a new category

---
+++ BudgetApp.Application/Features/Categories/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,11 @@
+﻿using BudgetApp.Application.Features.Categories.DTOs;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Categories.Commands;
+
+public class CreateCategoryCommand : IRequest<CategoryDto>
+{
+    public Guid BudgetId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public decimal AllocatedAmount { get; set; }
+}
