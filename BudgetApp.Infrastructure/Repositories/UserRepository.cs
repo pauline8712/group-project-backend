8185805d15f7c0903f94d6104afed89c14140eeb
Pauline <plinekaeorg@gmail.com>
2026-05-12 17:32:17 +0200

    feat: add GetByIdAsync method to UserRepository

--- BudgetApp.Infrastructure/Repositories/UserRepository.cs
+++ BudgetApp.Infrastructure/Repositories/UserRepository.cs
@@ -20,5 +20,13 @@ public class UserRepository : IUserRepository
         return await _context.Users.ToListAsync();
     }
 
+    // Hämtar en specifik användare baserat på Id
+    public async Task<User?> GetByIdAsync(Guid id)
+    {
+        return await _context.Users
+            .FirstOrDefaultAsync(u => u.Id == id);
+    }
+
+
 
 }
