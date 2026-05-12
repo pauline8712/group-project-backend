17d8f0a5a8bb4842d7349f5a0d11e3d5af4cf794
Pauline <plinekaeorg@gmail.com>
2026-05-12 21:48:19 +0200

    add getuserbyidqueryhandler with iuserrepository dependency

---
+++ BudgetApp.Application/Features/Users/Queries/GetUserByIdQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using BudgetApp.Application.Features.Users.DTOs;
+using BudgetApp.Application.Interfaces;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Users.Queries;
+
+// Hanterar GetUserByIdQuery — hämtar en specifik användare från databasen
+public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserDto?>
+{
+    private readonly IUserRepository _userRepository;
+
+    public GetUserByIdQueryHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+}
