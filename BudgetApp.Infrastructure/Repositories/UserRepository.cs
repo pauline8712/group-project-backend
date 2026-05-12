76a701d169bea33f4bb5297edc1c6b05436f768d
Pauline <plinekaeorg@gmail.com>
2026-05-12 17:29:59 +0200

    feat: add UserRepository with EF Core implementation

---
+++ BudgetApp.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,17 @@
+﻿using BudgetApp.Application.Interfaces;
+using BudgetApp.Domain.Entities;
+using BudgetApp.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetApp.Infrastructure.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly AppDbContext _context;
+
+    public UserRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+}
