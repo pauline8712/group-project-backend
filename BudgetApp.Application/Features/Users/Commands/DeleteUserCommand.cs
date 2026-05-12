4ca92e257a9754e301e7c0cd591a787e16374430
Pauline <plinekaeorg@gmail.com>
2026-05-12 21:37:22 +0200

    add deleteusercommand with Id property

---
+++ BudgetApp.Application/Features/Users/Commands/DeleteUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BudgetApp.Application.Features.Users.Commands;
+
+// Command för att ta bort en användare baserat på Id
+public class DeleteUserCommand : IRequest<bool>
+{
+    public Guid Id { get; set; }
+}
