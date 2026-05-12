bb309c543b9afc28374dba4fc4c9422df6eb958a
Pauline <plinekaeorg@gmail.com>
2026-05-12 21:26:14 +0200

    add CreateuserCommandHandler with IUserRepository dependency

---
+++ BudgetApp.Application/Features/Users/Commands/CreateUserCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using BudgetApp.Application.Features.Users.DTOs;
+using BudgetApp.Application.Interfaces;
+using BudgetApp.Domain.Entities;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Users.Commands
+
+// Hanterar CreateUserCommand — skapar en ny användare i databasen
+public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, UserDto>
+{
+    private readonly IUserRepository _userRepository;
+
+    public CreateUserCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+}
