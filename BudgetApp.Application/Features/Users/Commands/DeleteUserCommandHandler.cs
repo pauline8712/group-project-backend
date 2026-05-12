f688f0b978223c74ebd98b2a2f4728b9d7b46664
Pauline <plinekaeorg@gmail.com>
2026-05-12 21:41:56 +0200

    feat: add DeleteUserCommandHandler with IUserRepository dependency

---
+++ BudgetApp.Application/Features/Users/Commands/DeleteUserCommandHandler.cs
@@ -0,0 +1,15 @@
+﻿using BudgetApp.Application.Interfaces;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Users.Commands;
+
+// Hanterar DeleteUserCommand — tar bort en användare från databasen
+public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+{
+    private readonly IUserRepository _userRepository;
+
+    public DeleteUserCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+}
