e33a3865f10515cd746df7acf3a4bce88789d9d1
Pauline <plinekaeorg@gmail.com>
2026-05-12 21:52:30 +0200

    add getallusersqueryhandler with iuserrepository dependency

---
+++ BudgetApp.Application/Features/Users/Queries/GetAllUsersQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using BudgetApp.Application.Features.Users.DTOs;
+using BudgetApp.Application.Interfaces;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Users.Queries;
+
+// Hanterar GetAllUsersQuery — hämtar alla användare från databasen
+public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, List<UserDto>>
+{
+    private readonly IUserRepository _userRepository;
+
+    public GetAllUsersQueryHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+}
