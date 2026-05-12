e6c44ec0ea369a02eabe3c5e8c52899ebb44d786
Pauline <plinekaeorg@gmail.com>
2026-05-12 21:34:55 +0200

    add UpdateUserCommandHandler with IUserRepository dependency

---
+++ BudgetApp.Application/Features/Users/Commands/UpdateUserCommandHandler.cs
@@ -0,0 +1,16 @@
+﻿using BudgetApp.Application.Features.Users.DTOs;
+using BudgetApp.Application.Interfaces;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Users.Commands;
+
+// Hanterar UpdateUserCommand — uppdaterar en befintlig användare i databasen
+public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, UserDto>
+{
+    private readonly IUserRepository _userRepository;
+
+    public UpdateUserCommandHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+}
