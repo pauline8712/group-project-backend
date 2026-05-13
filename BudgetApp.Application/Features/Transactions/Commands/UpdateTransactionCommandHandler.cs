a5b525a1e375c39e04b3f9f1aaf5355dc022ee6d
Pauline <plinekaeorg@gmail.com>
2026-05-13 15:17:17 +0200

    created the class for UpdatetransactionCommandHandler that is inheriting code from  MediatR. With a constructor added

--- BudgetApp.Application/Features/Transactions/Commands/UpdateTransactionCommandHandler.cs
+++ BudgetApp.Application/Features/Transactions/Commands/UpdateTransactionCommandHandler.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BudgetApp.Application.Features.Transactions.DTOs;
+using BudgetApp.Application.Interfaces;
+using MediatR;
 
-namespace BudgetApp.Application.Features.Transactions.Commands
+namespace BudgetApp.Application.Features.Transactions.Commands;
+
+// Hanterar UpdateTransactionCommand — uppdaterar en befintlig transaktion i databasen
+public class UpdateTransactionCommandHandler : IRequestHandler<UpdateTransactionCommand, TransactionDto>
 {
-    internal class UpdateTransactionCommandHandler
+    private readonly ITransactionRepository _transactionRepository;
+
+    // ITransactionRepository injiceras via konstruktorn
+    public UpdateTransactionCommandHandler(ITransactionRepository transactionRepository)
     {
+        _transactionRepository = transactionRepository;
     }
 }
