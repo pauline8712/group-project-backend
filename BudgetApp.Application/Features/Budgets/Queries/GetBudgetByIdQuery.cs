5944ea08aa92a17150f32fb7cab8a7d61f662802
Pauline <plinekaeorg@gmail.com>
2026-05-13 09:09:47 +0200

    feat: add GetBudgetByIdQuery with Id property

---
+++ BudgetApp.Application/Features/Budgets/Queries/GetBudgetByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using BudgetApp.Application.Features.Budgets.DTOs;
+using MediatR;
+
+namespace BudgetApp.Application.Features.Budgets.Queries;
+
+// Query för att hämta en specifik budget baserat på Id
+public class GetBudgetByIdQuery : IRequest<BudgetDto?>
+{
+    public Guid Id { get; set; }
+}
