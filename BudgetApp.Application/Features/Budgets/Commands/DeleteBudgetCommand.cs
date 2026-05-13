fe8f6901cc9d737262aa7309e8759b48fd32ca74
Pauline <plinekaeorg@gmail.com>
2026-05-13 09:06:08 +0200

    feat: add DeleteBudgetCommand with Id property

---
+++ BudgetApp.Application/Features/Budgets/Commands/DeleteBudgetCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace BudgetApp.Application.Features.Budgets.Commands;
+
+// Command för att ta bort en budget baserat på Id
+public class DeleteBudgetCommand : IRequest<bool>
+{
+    public Guid Id { get; set; }
+}
