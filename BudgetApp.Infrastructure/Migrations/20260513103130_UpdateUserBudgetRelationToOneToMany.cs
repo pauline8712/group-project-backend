27a8e37c7992e6855301ef607d4c632cf8395f2b
Pauline <plinekaeorg@gmail.com>
2026-05-13 12:35:43 +0200

    Database updated with 1 to many relation between user and budget

---
+++ BudgetApp.Infrastructure/Migrations/20260513103130_UpdateUserBudgetRelationToOneToMany.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BudgetApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserBudgetRelationToOneToMany : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Budgets_UserId",
+                table: "Budgets");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budgets_UserId",
+                table: "Budgets",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Budgets_UserId",
+                table: "Budgets");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Budgets_UserId",
+                table: "Budgets",
+                column: "UserId",
+                unique: true);
+        }
+    }
+}
