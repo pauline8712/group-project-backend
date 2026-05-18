a2f2ef2151e02fd93d41b2c68b06568f3908de2b
Pauline <plinekaeorg@gmail.com>
2026-05-18 15:00:21 +0200

    feat: add CreateTransactionCommandValidator with FluentValidation rules

---
+++ BudgetApp.Application/Features/Transactions/Validators/CreateTransactionCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using BudgetApp.Application.Features.Transactions.Commands;
+using FluentValidation;
+
+namespace BudgetApp.Application.Features.Transactions.Validators;
+
+// Validerar CreateTransactionCommand innan den hanteras av CreateTransactionCommandHandler
+public class CreateTransactionCommandValidator : AbstractValidator<CreateTransactionCommand>
+{
+    public CreateTransactionCommandValidator()
+    {
+        // CategoryId måste vara ett giltigt GUID — får inte vara tomt
+        RuleFor(x => x.CategoryId)
+            .NotEmpty()
+            .WithMessage("CategoryId krävs");
+
+        // Amount måste vara större än 0
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+            .WithMessage("Belopp måste vara större än 0");
+
+        // Type måste vara antingen Expense eller Income
+        RuleFor(x => x.Type)
+            .NotEmpty()
+            .Must(t => t == "Expense" || t == "Income")
+            .WithMessage("Typ måste vara 'Expense' eller 'Income'");
+
+        // Date får inte vara tomt
+        RuleFor(x => x.Date)
+            .NotEmpty()
+            .WithMessage("Datum krävs");
+    }
+}
